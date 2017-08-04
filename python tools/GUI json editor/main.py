982cc6a2e25691483aa44bea3bef5ee7318f953f
Edmund King <ek7g14@soton.ac.uk>
2017-08-04 10:51:17 +0100

    Changed the schema to a json file, updated code accordingly

--- python tools/GUI json editor/main.py
+++ python tools/GUI json editor/main.py
@@ -198,66 +198,10 @@ class DataManager:
 		self.changed = True
 	
 	#method to load the schema from file into a dict
-	def __loadSchema(self, schemaLoc):
-	
-		#read the file
-		with open(schemaLoc) as f:
-			s = f.read()
-		
-		#split the file into lines
-		schemaArray = s.split('\n')
-		
-		#create the schema obj
-		schemaObj = {}
-		#array showing the current indexes we are working at
-		currIndexes = []
-		
-		#for each line in the schema, check how many tabs are at the front of the line
-		for line in schemaArray:
-			line = line.rstrip()
-			
-			#if there are fewer tabs than indexes, then we have moved up a level in the dictionary, so remove the last index
-			if (line.count('\t') < len(currIndexes)):
-				currIndexes.pop()
-				
-			#if there are more tabs than items in the schema, then the schema is broken
-			elif (line.count('\t') > len(currIndexes)):
-				print("You may have broken the Schema")
-			
-			line = line.strip()
-			
-			#if the final char in the line is a :, then we have to add a new level to the dict
-			if (line[-1] == ":"):
-				self.__addToSchema(schemaObj, currIndexes, line[:-1], {})
-				currIndexes.append(line[:-1])
-				
-			#if not then we are adding a property to the indexed dict
-			else:
-				#only split on the first :
-				property = line.split(':',1)
-				self.__addToSchema(schemaObj, currIndexes, property[0].strip(), property[1].strip())
-		print(schemaObj)
-		#TODO: get keys from schema obj in a recusive manner into an array (ignore keys linked to other dicts)
-		for key in schemaObj:
-			print(key)
-		
-	#method for recursively adding new items to the schema object
-	def __addToSchema(self, obj, indexes, property, val = {}):
-	
-		#if there are indexes, then we need to traverse the dict to the sepcified loc
-		if (indexes):
-		
-			#split the indexes array into the top element and the rest
-			restIndex = indexes.copy()
-			topIndex = restIndex.pop(0)
-			
-			#update the dict referenced, with the result of the recusive call
-			obj[topIndex].update(self.__addToSchema(obj[topIndex], restIndex, property, val))
-			return obj
-		
-		#if no indexes, then just add the property/val to the dict at this level
-		obj[property] = val
-		return obj
+	def __loadSchema(self,schemaLoc):
+		if(schemaLoc):
+			with open(schemaLoc) as f:
+				self.schema = json.load(f)
 		
 	def getAllRows(self):
 		return self.allData
@@ -286,6 +230,6 @@ root = Tk()
 #self.grab_release()
 #self.top = tki.Toplevel()
 
-dm = DataManager(os.path.dirname(os.path.realpath(__file__))+"\schema.txt")
+dm = DataManager(os.path.dirname(os.path.realpath(__file__))+"\schema.json")
 app = Home(root, dm)
 root.mainloop()
